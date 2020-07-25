6bfef96d8c491053885f0df5fba246024b97caf2
ciCCic <koraypoyraz1@gmail.com>
2020-07-25 14:14:00 +0200

    upgrade versions added host to ApiStore.kt

--- app/src/main/java/com/koray/nrcnewsapp/core/network/ApiStore.kt
+++ app/src/main/java/com/koray/nrcnewsapp/core/network/ApiStore.kt
@@ -7,5 +7,5 @@ object ApiStore {
     const val GAMES = "games"
     const val CATEGORY_ITEMS = "categories"
 //    const val API_LOCAL = "http://192.168.178.11:8080";
-    const val API_LOCAL = "http://192.168.0.239:8080";
+    const val API_LOCAL = "http://williamhall.ddns.net:5011";
 }
